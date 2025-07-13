de113fac4e34c1a33a565367c04c76def69f0570
ZengHui Tang <1455434664@qq.com>
2025-07-13 23:34:29 +0800

    Quartz sync: Jul 13, 2025, 11:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,14 +9,14 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitleSuffix: "-三只小猫",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "zh-CN",
+    baseUrl: "/blogs",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
